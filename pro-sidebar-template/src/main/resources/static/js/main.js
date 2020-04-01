c19e6c297b8b8d4e8d69adf86701af4899ee83bf
Ferry L.H. <ferry.hutapea@myrepublic.net>
2020-04-01 17:33:12 +0700

    add page header

--- pro-sidebar-template/src/main/resources/static/js/main.js
+++ pro-sidebar-template/src/main/resources/static/js/main.js
@@ -1,33 +1,35 @@
 jQuery(function ($) {
 
-    // Dropdown menu
-    $(".sidebar-dropdown > a").click(function () {
-        $(".sidebar-submenu").slideUp(200);
-        if ($(this).parent().hasClass("active")) {
-            $(".sidebar-dropdown").removeClass("active");
-            $(this).parent().removeClass("active");
-        } else {
-            $(".sidebar-dropdown").removeClass("active");
-            $(this).next(".sidebar-submenu").slideDown(200);
-            $(this).parent().addClass("active");
-        }
+    $(".sidebar-dropdown > a").click(function() {
+  $(".sidebar-submenu").slideUp(200);
+  if (
+    $(this)
+      .parent()
+      .hasClass("active")
+  ) {
+    $(".sidebar-dropdown").removeClass("active");
+    $(this)
+      .parent()
+      .removeClass("active");
+  } else {
+    $(".sidebar-dropdown").removeClass("active");
+    $(this)
+      .next(".sidebar-submenu")
+      .slideDown(200);
+    $(this)
+      .parent()
+      .addClass("active");
+  }
+});
 
-    });
+$("#close-sidebar").click(function() {
+  $(".page-wrapper").removeClass("toggled");
+});
+$("#show-sidebar").click(function() {
+  $(".page-wrapper").addClass("toggled");
+});
 
-    //toggle sidebar
-    $("#toggle-sidebar").click(function () {
-		$(this).toggleClass('is-active');
-        $(".page-wrapper").toggleClass("toggled");
-    });
+
    
-    //custom scroll bar is only used on desktop
-    if (!/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
-        $(".sidebar-content").mCustomScrollbar({
-            axis: "y",
-            autoHideScrollbar: true,
-            scrollInertia: 300
-        });
-        $(".sidebar-content").addClass("desktop");
-    }
-    
+   
 });

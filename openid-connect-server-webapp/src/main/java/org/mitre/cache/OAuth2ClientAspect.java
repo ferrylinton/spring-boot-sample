979e92a45c143f0f55c76a823dae2e5df0d294e7
Ferry L.H. <ferry.hutapea@myrepublic.net>
2020-04-23 22:12:32 +0700

    update cache

--- openid-connect-server-webapp/src/main/java/org/mitre/cache/OAuth2ClientAspect.java
+++ openid-connect-server-webapp/src/main/java/org/mitre/cache/OAuth2ClientAspect.java
@@ -1,8 +1,12 @@
 package org.mitre.cache;
 
+
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.mitre.oauth2.model.ClientDetailsEntity;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,12 +23,13 @@ public class OAuth2ClientAspect {
 	
 	@Autowired
 	private Cache oauth2ClientCache;
-	
+
 	@Around("execution(* org.mitre.oauth2.repository.impl.JpaOAuth2ClientRepository.getClientByClientId(..)) && args(clientId)")
 	public Object getClientByClientId(ProceedingJoinPoint proceedingJoinPoint, String clientId) throws Throwable{
 		Object obj = null;
 		
 		try {
+			
 			
 			if (oauth2ClientCache.isKeyInCache(clientId)) {
 				logger.info("cache :: org.mitre.oauth2.repository.impl.JpaOAuth2ClientRepository.getClientByClientId() from cache");
@@ -43,6 +48,35 @@ public class OAuth2ClientAspect {
 		}
 		
 		return obj;
+	}
+	
+	@Before("execution(* org.mitre.oauth2.repository.impl.JpaOAuth2ClientRepository.updateClient(..)) && args(id, client)")
+	public void updateClient(JoinPoint joinPoint, Long id, ClientDetailsEntity client){
+		try {
+			
+			if (oauth2ClientCache.isKeyInCache(client.getClientId())) {
+				logger.info ("cache :: org.mitre.oauth2.repository.impl.JpaOAuth2ClientRepository.updateClient() :: remove [clientId='{}'] from cache", client.getClientId());
+				
+	            oauth2ClientCache.remove(client.getClientId());
+	        }
+			
+		} catch (Throwable e) {
+			logger.error(e.getLocalizedMessage(), e);
+		}
+	}
+	
+	@Before("execution(* org.mitre.oauth2.repository.impl.JpaOAuth2ClientRepository.deleteClient(..)) && args(client)")
+	public void deleteClient(JoinPoint joinPoint, ClientDetailsEntity client){
+		try {
+
+			if (oauth2ClientCache.isKeyInCache(client.getClientId())) {
+				logger.info ("cache :: org.mitre.oauth2.repository.impl.JpaOAuth2ClientRepository.deleteClient() :: remove [clientId='{}'] from cache", client.getClientId());
+				oauth2ClientCache.remove(client.getClientId());
+	        }
+			
+		} catch (Throwable e) {
+			logger.error(e.getLocalizedMessage(), e);
+		}
 	}
 
 }

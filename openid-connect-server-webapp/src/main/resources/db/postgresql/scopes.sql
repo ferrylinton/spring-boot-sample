a2dc42c73663e7b9689c4ce40e505a840370d85c
Ferry L. H. <ferrylinton@gmail.com>
2020-04-27 02:09:17 +0700

    update sql

---
+++ openid-connect-server-webapp/src/main/resources/db/postgresql/scopes.sql
@@ -0,0 +1,15 @@
+--
+-- Insert scope information into the temporary tables.
+-- 
+
+INSERT INTO system_scope_TEMP (scope, description, icon, restricted, default_scope, structured, structured_param_description) VALUES
+  ('openid', 'log in using your identity', 'user', false, true, false, null),
+  ('profile', 'basic profile information', 'list-alt', false, true, false, null),
+  ('email', 'email address', 'envelope', false, true, false, null),
+  ('address', 'physical address', 'home', false, true, false, null),
+  ('phone', 'telephone number', 'bell', false, true, false, null),
+  ('offline_access', 'offline access', 'time', false, false, false, null);
+
+INSERT INTO system_scope
+	(scope, description, icon, restricted, default_scope, structured, structured_param_description)
+	SELECT scope, description, icon, restricted, default_scope, structured, structured_param_description FROM system_scope_TEMP;
---
+++ openid-connect-server-webapp/src/main/resources/db/postgresql/users.sql
@@ -0,0 +1,101 @@
+--
+-- Insert user information into the temporary tables. To add users to the HSQL database, edit things here.
+-- 
+
+INSERT INTO users_TEMP (username, password, enabled) VALUES
+  ('admin','password',true),
+  ('admin1','password',true),
+  ('admin2','password',true),
+  ('admin3','password',true),
+  ('admin4','password',true),
+  ('admin5','password',true),
+  ('admin6','password',true),
+  ('admin7','password',true),
+  ('admin8','password',true),
+  ('admin9','password',true),
+  ('admin10','password',true),
+  ('user','password',true),
+  ('user1','password',true),
+  ('user2','password',true),
+  ('user3','password',true),
+  ('user4','password',true),
+  ('user5','password',true),
+  ('user6','password',true),
+  ('user7','password',true),
+  ('user8','password',true),
+  ('user9','password',true),
+  ('user10','password',true);
+
+
+INSERT INTO authorities_TEMP (username, authority) VALUES
+  ('admin','ROLE_ADMIN'),
+  ('admin','ROLE_USER'),
+  ('admin1','ROLE_ADMIN'),
+  ('admin1','ROLE_USER'),
+  ('admin2','ROLE_ADMIN'),
+  ('admin2','ROLE_USER'),
+  ('admin3','ROLE_ADMIN'),
+  ('admin3','ROLE_USER'),
+  ('admin4','ROLE_ADMIN'),
+  ('admin4','ROLE_USER'),
+  ('admin5','ROLE_ADMIN'),
+  ('admin5','ROLE_USER'),
+  ('admin6','ROLE_ADMIN'),
+  ('admin6','ROLE_USER'),
+  ('admin7','ROLE_ADMIN'),
+  ('admin7','ROLE_USER'),
+  ('admin8','ROLE_ADMIN'),
+  ('admin8','ROLE_USER'),
+  ('admin9','ROLE_ADMIN'),
+  ('admin9','ROLE_USER'),
+  ('admin10','ROLE_ADMIN'),
+  ('admin10','ROLE_USER'),
+  ('user','ROLE_USER'),
+  ('user1','ROLE_USER'),
+  ('user2','ROLE_USER'),
+  ('user3','ROLE_USER'),
+  ('user4','ROLE_USER'),
+  ('user5','ROLE_USER'),
+  ('user6','ROLE_USER'),
+  ('user7','ROLE_USER'),
+  ('user8','ROLE_USER'),
+  ('user9','ROLE_USER'),
+  ('user10','ROLE_USER');
+    
+-- By default, the username column here has to match the username column in the users table, above
+INSERT INTO user_info_TEMP (sub, preferred_username, name, email, email_verified) VALUES
+  ('90342.ASDFJWFA','admin','Demo Admin','admin@example.com', true),
+  ('90311.ASDFJWFA','admin1','Demo Admin1','admin1@example.com', true),
+  ('90322.ASDFJWFA','admin2','Demo Admin2','admin2@example.com', true),
+  ('90333.ASDFJWFA','admin3','Demo Admin3','admin3@example.com', true),
+  ('90344.ASDFJWFA','admin4','Demo Admin4','admin4@example.com', true),
+  ('90355.ASDFJWFA','admin5','Demo Admin5','admin5@example.com', true),
+  ('90366.ASDFJWFA','admin6','Demo Admin6','admin6@example.com', true),
+  ('90377.ASDFJWFA','admin7','Demo Admin7','admin7@example.com', true),
+  ('90388.ASDFJWFA','admin8','Demo Admin8','admin8@example.com', true),
+  ('90399.ASDFJWFA','admin9','Demo Admin9','admin9@example.com', true),
+  ('90300.ASDFJWFA','admin10','Demo Admin10','admin10@example.com', true),
+  ('01921.FLANRJQW','user','Demo User','user@example.com', true),
+  ('01911.FLANRJQW','user1','Demo User1','user1@example.com', true),
+  ('01922.FLANRJQW','user2','Demo User2','user2@example.com', true),
+  ('01933.FLANRJQW','user3','Demo User3','user3@example.com', true),
+  ('01944.FLANRJQW','user4','Demo User4','user4@example.com', true),
+  ('01955.FLANRJQW','user5','Demo User5','user5@example.com', true),
+  ('01966.FLANRJQW','user6','Demo User6','user6@example.com', true),
+  ('01977.FLANRJQW','user7','Demo User7','user7@example.com', true),
+  ('01988.FLANRJQW','user8','Demo User8','user8@example.com', true),
+  ('01999.FLANRJQW','user9','Demo User9','user9@example.com', true),
+  ('01900.FLANRJQW','user10','Demo User10','user10@example.com', true);
+
+ 
+INSERT INTO users
+	(username, password, enabled)
+	SELECT username, password, enabled FROM users_TEMP;
+	
+INSERT INTO authorities
+	(username,authority)
+	SELECT username, authority FROM authorities_TEMP;
+	
+INSERT INTO user_info
+	(sub, preferred_username, name, email, email_verified)
+	SELECT sub, preferred_username, name, email, email_verified FROM user_info_TEMP;

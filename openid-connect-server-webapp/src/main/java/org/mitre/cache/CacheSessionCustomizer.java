448bf81cd0d9a2a6ab4b3549343f9e90feeab2de
Ferry L. H. <ferrylinton@gmail.com>
2020-05-11 19:09:55 +0700

    edit SessionCustomizer

--- openid-connect-server-webapp/src/main/java/org/mitre/cache/CacheSessionCustomizer.java
+++ openid-connect-server-webapp/src/main/java/org/mitre/cache/CacheSessionCustomizer.java
@@ -8,13 +8,20 @@ import org.eclipse.persistence.config.CacheIsolationType;
 import org.eclipse.persistence.config.SessionCustomizer;
 import org.eclipse.persistence.descriptors.ClassDescriptor;
 import org.eclipse.persistence.sessions.Session;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-public class SessionCustomizerImpl implements SessionCustomizer {
+public class CacheSessionCustomizer implements SessionCustomizer {
+	
+	private static final Logger logger = LoggerFactory.getLogger(CacheSessionCustomizer.class);
 
 	private static final List<String> TABLES = Arrays.asList("client_details", "approved_site", "saved_registered_client", "resource_set", "blacklisted_site", "pairwise_identifier", "permission_ticket", "refresh_token", "policy", "authorization_code", "system_scope", "whitelisted_site", "address", "authentication_holder", "access_token", "saved_user_auth", "claim", "permission");
 	
 	@Override
 	public void customize(Session session) throws Exception {
+		logger.info("######################################################");
+		logger.info("################## CACHE IS ENABLE ###################");
+		logger.info("######################################################");
 		Collection<ClassDescriptor> classDescriptors = session.getDescriptors().values();
 		
 		for (ClassDescriptor classDescriptor : classDescriptors) {
